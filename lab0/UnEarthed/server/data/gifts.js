6fa7bd5796271af461512b36d7be15fd5e79bf68
ThomasH07 <thomash0720@gmail.com>
2025-09-18 19:51:59 -0400

    step7

--- lab0/UnEarthed/server/data/gifts.js
+++ lab0/UnEarthed/server/data/gifts.js
@@ -1,15 +1,26 @@
 
-giftData = {
-    "gifts" : [
+const giftData = 
+    [
         {
-        "id": "1",
-        "name": "teddy bear",
-        "pricePoint": "$4",
-        "audience": "children",
-        "image": "example image of a teady bear .com",
-        "description": "a soft and cuddly teddy bear",
-        "submittedBy": "John Doe",
-        "submittedOn": "2025-08-18",
+            "id": "1",
+            "name": "teddy bear",
+            "pricePoint": "$",
+            "audience": "children",
+            "image": "example image of a teady bear .com",
+            "description": "a soft and cuddly teddy bear",
+            "submittedBy": "John Doe",
+            "submittedOn": "2025-08-18",
+        },
+        {
+            "id": "2",
+            "name": "spinner",
+            "pricePoint": "$",
+            "audience": "children",
+            "image": "example image of a spinner .com",
+            "description": "a spinner fr",
+            "submittedBy": "Jane Doe",
+            "submittedOn": "2022-01-12",
         }
-    ]
-}
+    ];
+
+export default giftData;
--- lab0/UnEarthed/src/server.js
+++
@@ -1,1 +0,0 @@
-import giftsRouter from './routes/gifts.js'
