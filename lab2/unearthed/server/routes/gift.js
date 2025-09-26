88d17e21d43b1486b4bc9a6accad7ead0f1d149a
ThomasH07 <thomash0720@gmail.com>
2025-09-26 16:02:17 -0400

    90% done

---
+++ lab2/unearthed/server/routes/gift.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import GiftsController from '../controllers/gifts.js'
+
+const router = express.Router()
+
+router.get('/', GiftsController.getGifts)
+
+export default router
